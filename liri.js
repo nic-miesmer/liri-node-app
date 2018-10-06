6478ea948babcbd5acdd5768814c7f3b06dca739
nmiesmer <nic.miesmer@segmint.com>
2018-10-06 13:37:23 -0400

    added code to require all needed items I hope...

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var Request = require("request");
+var Moment = require("moment");
+
 var spotify = new Spotify(keys.spotify);
+
