019d82e9e720a5da0512acee64ba4ad539105cf0
nmiesmer <nic.miesmer@segmint.com>
2018-10-06 23:26:02 -0400

    data returned for band-this need to format date

--- liri.js
+++ liri.js
@@ -11,17 +11,20 @@ var spotify = new Spotify(keys.spotify);
 // Here we incorporate the "request" npm package
 var request = require("request");
 
+
 var inputString = process.argv;
-
-
-
 var command = process.argv[2];
 var search = process.argv[3];
 
 if(command === "movie-this"){
+
+    console.log(search);
     
+    // search = search.split(' ').join('+')
+
+    console.log(search);
     if(search == null){
-        search = "Mr.+Nobody"
+        search = "Mr.+Nobody";
     }
 
 // We then run the request module on a URL with a JSON
@@ -45,5 +48,31 @@ request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=trilogy&tom
 
 }
 });
+
+}
+
+else if(command === "concert-this") {
+
+    // We then run the request module on a URL with a JSON
+request("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp", function(error, response, body) {
+
+    // If there were no errors and the response code was 200 (i.e. the request was successful)...
+    if (!error && response.statusCode === 200) {
+  
+        var bandInfoJson = JSON.parse(body)
+        for( var i = 0; i < bandInfoJson.length; i++){
+            // console.log(bandInfoJson[i])
+            console.log(bandInfoJson[i].venue.name)
+            console.log(bandInfoJson[i].venue.city + ", " + bandInfoJson[i].venue.country)
+            console.log(bandInfoJson[i].datetime)
+            console.log("__________________________________")
+
+        }
+          
+
+  
+  }
+  });
+
 
 }
