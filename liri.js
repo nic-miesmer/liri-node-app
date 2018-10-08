31b8a7759bf00de957bdea017daeda777e9dedf2
nmiesmer <nic.miesmer@segmint.com>
2018-10-07 22:23:53 -0400

    do this funciton working, but errors passing values

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var Request = require("request");
 var Moment = require("moment");
+var fs = require('fs');
+
 
 var spotify = new Spotify(keys.spotify);
 
@@ -16,71 +18,61 @@ var inputString = process.argv;
 var command = process.argv[2];
 var search = process.argv[3];
 
-if(command === "movie-this"){
 
-    console.log(search);
-    
-    // search = search.split(' ').join('+')
+var movieThis = function(search){
+    if(command === "movie-this"){
 
-    console.log(search);
-    if(search == null){
-        search = "Mr.+Nobody";
-    }
-
-// We then run the request module on a URL with a JSON
-request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=trilogy&tomatoes=true", function(error, response, body) {
-
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-  if (!error && response.statusCode === 200) {
-
-    // Then we print out the imdbRating
-    console.log("The movie's title is: " + JSON.parse(body).Title);
-    console.log("The movie's title is: " + JSON.parse(body).Year);
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-    console.log("The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).tomatoMeter);
-    console.log("The movie's was produced in: " + JSON.parse(body).Country);
-    console.log("The movie's original language is: " + JSON.parse(body).Country);
-    console.log("The movie's plot: " + JSON.parse(body).Plot);
-    console.log("The movie's actors: " + JSON.parse(body).Actors);
-
-
-
-
-}
-});
-
-}
-
-else if(command === "concert-this") {
-
-    // We then run the request module on a URL with a JSON
-request("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp", function(error, response, body) {
-
-    // If there were no errors and the response code was 200 (i.e. the request was successful)...
-    if (!error && response.statusCode === 200) {
-  
-        var bandInfoJson = JSON.parse(body)
-        for( var i = 0; i < bandInfoJson.length; i++){
-            // console.log(bandInfoJson[i])
-            console.log(bandInfoJson[i].venue.name)
-            console.log(bandInfoJson[i].venue.city + ", " + bandInfoJson[i].venue.country)
-            console.log(bandInfoJson[i].datetime)
-            console.log("__________________________________")
-
+        if(search == null){
+            search = "Mr.+Nobody";
         }
-          
+    
+    // We then run the request module on a URL with a JSON
+    request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=trilogy&tomatoes=true", function(error, response, body) {
+    
+      // If there were no errors and the response code was 200 (i.e. the request was successful)...
+      if (!error && response.statusCode === 200) {
+    
+        // Then we print out the imdbRating
+        console.log("The movie's title is: " + JSON.parse(body).Title);
+        console.log("The movie's title is: " + JSON.parse(body).Year);
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        console.log("The movie's Rotten Tomatoes Rating is: " + JSON.parse(body).tomatoMeter);
+        console.log("The movie's was produced in: " + JSON.parse(body).Country);
+        console.log("The movie's original language is: " + JSON.parse(body).Country);
+        console.log("The movie's plot: " + JSON.parse(body).Plot);
+        console.log("The movie's actors: " + JSON.parse(body).Actors);
+    
+    }
+    });
 
-  
-  }
-  });
+}
+}
 
+var concertThis = function(seach) {
 
+    if(command === "concert-this") {
+
+        // We then run the request module on a URL with a JSON
+    request("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp", function(error, response, body) {
+    
+        // If there were no errors and the response code was 200 (i.e. the request was successful)...
+        if (!error && response.statusCode === 200) {
+      
+            var bandInfoJson = JSON.parse(body)
+            for( var i = 0; i < bandInfoJson.length; i++){
+                // console.log(bandInfoJson[i])
+                console.log(bandInfoJson[i].venue.name)
+                console.log(bandInfoJson[i].venue.city + ", " + bandInfoJson[i].venue.country)
+                console.log(bandInfoJson[i].datetime)
+                console.log("__________________________________")
+            }
+        }
+      });
+    }
 }
 
 
-else if(command === "spotify-this-song") {
-
-
+var spotifyThis = function(search) {
     if(search == null){
         noSpecialCharSearch = 'the sign' 
     }
@@ -123,4 +115,59 @@ else if(command === "spotify-this-song") {
         console.log("-----------------------------------------------------------------------------")
       }
       });
+
 }
+
+var doThis = function(){
+
+    fs.readFile('./random.txt', 'utf8', function (error, data) {
+		if (error) {
+			console.log('ERROR: Reading file' + error);
+			return;
+		} else {
+			// Split out the command name and the parameter name
+			var cmdString = data.split(',');
+			var command = cmdString[0].trim();
+			var search = cmdString[1].trim();
+
+            console.log(command);
+
+            console.log(search);
+
+			  switch(command) {
+				case 'concert-this':
+                    concertThis(search); 
+					break;
+
+				case 'spotify-this-song':
+					spotifyThis(search);
+					break;
+
+				case 'movie-this':
+                    movieThis(search);
+                    break;
+        }
+    }
+	});
+}
+
+
+
+
+  switch(command) {
+				case 'concert-this':
+                    concertThis(search); 
+					break;
+
+				case 'spotify-this-song':
+					spotifyThis(search);
+					break;
+
+				case 'movie-this':
+                    movieThis(search);
+                    break;
+                    
+                case 'do-what-it-says':
+                    doThis();
+					break;
+			}
